1034f7ce346146ef905e9b65d22276953d60d628
JeanCGuedes <jean.carlo.guedes@gmail.com>
2023-01-23 16:12:48 -0300

    First Commit

---
+++ src/main/java/com/jeanbrianez/filialinfoapi/FilialInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeanbrianez.filialinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilialInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilialInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeanbrianez/filialinfoapi/FilialInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbrianez.filialinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilialInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
